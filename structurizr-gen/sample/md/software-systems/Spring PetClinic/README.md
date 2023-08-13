3f1358ed5d82c5fedecbcb06d032dd3c70b5124f
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-08-13 20:51:12 +0200

    Create tables of containers/components and connections of this software system

--- structurizr-gen/sample/md/software-systems/Spring PetClinic/README.md
+++ structurizr-gen/sample/md/software-systems/Spring PetClinic/README.md
@@ -9,14 +9,12 @@ description: System Context of Spring PetClinic
 
 [png](../../images/system_context%20Spring%20PetClinic.png) | [svg](../../images/system_context%20Spring%20PetClinic.svg)
 
-### Spring PetClinic - System Context
-
-description: System Context of Spring PetClinic
-![system_context Spring PetClinic](../../images/system_context%20Spring%20PetClinic.png)
-
-[png](../../images/system_context%20Spring%20PetClinic.png) | [svg](../../images/system_context%20Spring%20PetClinic.svg)
-
 ## Containers
+
+| Container | Description |
+| --- | --- |
+| Database | Stores information regarding the veterinarians, the clients, and their pets. |
+| Web Application | Allows employees to view and manage information regarding the veterinarians, the clients, and their pets. |
 
 ### Spring PetClinic - Containers
 
@@ -27,12 +25,50 @@ description: Container view of Spring PetClinic
 
 ## Components
 
+| Container | Component | Description |
+| --- | --- | --- |
+| Web Application | Web Service 1 |  |
+| Web Application | Web Service 2 |  |
+
 ### Spring PetClinic - Web Application - Components
 
 description: Component view of Web Application
 ![component Spring PetClinic Web Application](../../images/component%20Spring%20PetClinic%20Web%20Application.png)
 
 [png](../../images/component%20Spring%20PetClinic%20Web%20Application.png) | [svg](../../images/component%20Spring%20PetClinic%20Web%20Application.svg)
+
+## Relationships
+
+### Called by
+
+| Source Software System | Source Container | Source Component | Target Software System | Target Container | Target Component | Description |
+| --- | --- | --- | --- | --- | --- | --- |
+| App 2 | Web Application 2: []()%$&à\èéìù/ | Web Service 4 | Spring PetClinic | Web Application |  | test 1 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ | Web Service 4 | Spring PetClinic | Web Application | Web Service 1 | test 2 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ | Web Service 4 | Spring PetClinic | Web Application | Web Service 2 | test 1 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ | Web Service 4 | Spring PetClinic |  |  | test 1 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ |  | Spring PetClinic | Web Application |  | test 1 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ |  | Spring PetClinic | Web Application | Web Service 1 | test 2 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ |  | Spring PetClinic | Web Application | Web Service 2 | test 1 |
+| App 2 | Web Application 2: []()%$&à\èéìù/ |  | Spring PetClinic |  |  | test 1 |
+| App 2 |  |  | Spring PetClinic | Web Application |  | test 1 |
+| App 2 |  |  | Spring PetClinic | Web Application | Web Service 1 | test 2 |
+| App 2 |  |  | Spring PetClinic | Web Application | Web Service 2 | test 1 |
+| App 2 |  |  | Spring PetClinic |  |  | test 1 |
+| Spring PetClinic | Web Application | Web Service 1 | Spring PetClinic | Database |  | Reads from and writes to |
+| Spring PetClinic | Web Application | Web Service 2 | Spring PetClinic | Web Application | Web Service 1 |  |
+| Spring PetClinic | Web Application |  | Spring PetClinic | Database |  | Reads from and writes to |
+| User |  |  | Spring PetClinic | Web Application |  | Access to user data |
+| User |  |  | Spring PetClinic | Web Application | Web Service 2 | Access to user data |
+| User |  |  | Spring PetClinic |  |  | Access to user data |
+
+### Calls
+
+| Source Software System | Source Container | Source Component | Target Software System | Target Container | Target Component | Description |
+| --- | --- | --- | --- | --- | --- | --- |
+| Spring PetClinic | Web Application | Web Service 1 | Spring PetClinic | Database |  | Reads from and writes to |
+| Spring PetClinic | Web Application | Web Service 2 | Spring PetClinic | Web Application | Web Service 1 |  |
+| Spring PetClinic | Web Application |  | Spring PetClinic | Database |  | Reads from and writes to |
 
 ## Deployments
 
--- structurizr-gen/src/main/java/it/vitalegi/structurizr/gen/markdown/SoftwareSystemPage.java
+++ structurizr-gen/src/main/java/it/vitalegi/structurizr/gen/markdown/SoftwareSystemPage.java
@@ -1,12 +1,17 @@
 package it.vitalegi.structurizr.gen.markdown;
 
+import com.structurizr.model.Element;
+import com.structurizr.model.Person;
+import com.structurizr.model.Relationship;
 import com.structurizr.model.SoftwareSystem;
 import com.structurizr.view.DeploymentView;
 import com.structurizr.view.StaticView;
 import com.structurizr.view.View;
 import it.vitalegi.structurizr.gen.model.MdContext;
+import it.vitalegi.structurizr.gen.util.MarkdownTable;
 import it.vitalegi.structurizr.gen.util.MarkdownUtil;
 import it.vitalegi.structurizr.gen.util.StringUtil;
+import it.vitalegi.structurizr.gen.util.StructurizrUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -54,8 +59,34 @@ public class SoftwareSystemPage {
         md.addViewImages(view, ctx, ctx.getSoftwareSystemPathToRoot());
     }
 
+    protected void createSectionCalledBy(MarkdownUtil md, SoftwareSystem ss) {
+        if (ctx.getRelationsToTree(ss).count() == 0) {
+            return;
+        }
+        md.h3("Called by");
+        relationshipTable(md, ctx.getRelationsToTree(ss));
+    }
+
+
+    protected void createSectionCalls(MarkdownUtil md, SoftwareSystem ss) {
+        if (ctx.getRelationsFromTree(ss).count() == 0) {
+            return;
+        }
+        md.h3("Calls");
+        relationshipTable(md, ctx.getRelationsFromTree(ss));
+    }
+
     protected void createSectionContainers(MarkdownUtil md, SoftwareSystem ss) {
-        md.h2("Containers");
+        if (ctx.getContainersSorted(ss).count() == 0) {
+            return;
+        }
+        var table = md.table("Container", "Description");
+        ctx.getContainersSorted(ss).forEach(c -> table.addRow(c.getName(), c.getDescription()));
+        table.build();
+        md.println();
+    }
+
+    protected void createSectionContainersDiagrams(MarkdownUtil md, SoftwareSystem ss) {
         createSection(md, ctx.getContainerViewsSorted().filter(v -> accept(v, ss)));
         md.println();
     }
@@ -67,14 +98,19 @@ public class SoftwareSystemPage {
     }
 
     protected void createSectionSystemComponents(MarkdownUtil md, SoftwareSystem ss) {
-        md.h2("Components");
-        createSection(md, ctx.getComponentViewsSorted().filter(v -> accept(v, ss)));
+        if (ctx.getComponentsSorted(ss).count() == 0) {
+            return;
+        }
+        var table = md.table("Container", "Component", "Description");
+        ctx.getComponentsSorted(ss).forEach(component -> {
+            table.addRow(component.getContainer().getName(), component.getName(), component.getDescription());
+        });
+        table.build();
         md.println();
     }
 
-    protected void createSectionSystemContexts(MarkdownUtil md, SoftwareSystem ss) {
-
-        createSection(md, ctx.getSystemContextViewsSorted().filter(v -> accept(v, ss)));
+    protected void createSectionSystemComponentsDiagrams(MarkdownUtil md, SoftwareSystem ss) {
+        createSection(md, ctx.getComponentViewsSorted().filter(v -> accept(v, ss)));
         md.println();
     }
 
@@ -88,10 +124,17 @@ public class SoftwareSystemPage {
             md.h1(ss.getName());
 
             md.h2("System Contexts");
-            createSectionSystemContexts(md, ss);
             createSectionSystemContextsDiagrams(md, ss);
+            md.h2("Containers");
             createSectionContainers(md, ss);
+            createSectionContainersDiagrams(md, ss);
+            md.h2("Components");
             createSectionSystemComponents(md, ss);
+            createSectionSystemComponentsDiagrams(md, ss);
+            md.h2("Relationships");
+            createSectionCalledBy(md, ss);
+            createSectionCalls(md, ss);
+
             createSectionDeployments(md, ss);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -103,6 +146,44 @@ public class SoftwareSystemPage {
             return view.getTitle();
         }
         return view.getName();
+    }
+
+    protected void relationshipTable(MarkdownUtil md, Stream<Relationship> relationships) {
+        var table = md.table("Source Software System", "Source Container", "Source Component", "Target Software " +
+                "System", "Target Container", "Target Component", "Description");
+        relationships.forEach(r -> relationshipTableRow(table, r));
+        table.build();
+        md.println();
+    }
+
+    protected void relationshipTableRow(MarkdownTable table, Relationship r) {
+        var src = r.getSource();
+        Element srcSS = StructurizrUtil.getParentSoftwareSystem(src);
+        Element srcCT = StructurizrUtil.getParentContainer(src);
+        Element srcCP = StructurizrUtil.getParentComponent(src);
+        if (src instanceof Person) {
+            srcSS = src;
+            srcCT = null;
+            srcCP = null;
+        }
+        var dest = r.getDestination();
+        Element destSS = StructurizrUtil.getParentSoftwareSystem(dest);
+        Element destCT = StructurizrUtil.getParentContainer(dest);
+        Element destCP = StructurizrUtil.getParentComponent(dest);
+        if (dest instanceof Person) {
+            destSS = dest;
+            destCT = null;
+            destCP = null;
+        }
+        table.addRow(getNameOrDefault(srcSS), getNameOrDefault(srcCT), getNameOrDefault(srcCP),
+                getNameOrDefault(destSS), getNameOrDefault(destCT), getNameOrDefault(destCP), r.getDescription());
+    }
+
+    private String getNameOrDefault(Element element) {
+        if (element != null) {
+            return element.getName();
+        }
+        return "";
     }
 
 }
